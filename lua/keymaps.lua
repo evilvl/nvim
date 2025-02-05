05be3eae1cca2c3f585097452bc9193b43988c07
Илья Евсеев <Ilya.Evseev_ext@lemanapro.ru>
2025-02-05 12:48:19 +0400

    6

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,16 +1,3 @@
 vim.g.mapleader = " "
 -- clipboard 
 vim.opt.clipboard = 'unnamedplus'
-if vim.fn.executable('wl-copy') == 1 then
-  vim.g.clipboard = {
-    name = 'wl-clipboard',
-    copy = {
-      ['+'] = 'wl-copy  --no-newline',
-      ['*'] = 'wl-copy --no-newline',
-    },
-    paste = {
-      ['+'] = 'wl-paste --no-newline',
-      ['*'] = 'wl-paste --no-newline',
-    },
-  }
-end
