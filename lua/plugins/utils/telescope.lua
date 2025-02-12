de9737997b2790cab802a5589d9532a63899a48b
Илья Евсеев <Ilya.Evseev_ext@lemanapro.ru>
2025-02-12 17:27:02 +0400

    8

--- lua/plugins/utils/telescope.lua
+++ lua/plugins/utils/telescope.lua
@@ -4,9 +4,8 @@ return {
   event = "VeryLazy",
   dependencies = {
     'nvim-lua/plenary.nvim',
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    { 
       'nvim-telescope/telescope-fzf-native.nvim',
-
       -- `build` is used to run some command when the plugin is installed/updated.
       -- This is only run then, not every time Neovim starts up.
       -- NOTE: nixCats: use lazyAdd to only run build steps if nix wasnt involved.
@@ -22,8 +21,6 @@ return {
       end),
     },
     { 'nvim-telescope/telescope-ui-select.nvim' },
-
-    -- Useful for getting pretty icons, but requires a Nerd Font.
     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   },
   config = function()
