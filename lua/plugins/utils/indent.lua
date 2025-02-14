07dba308221ee832875548d297653373c1e470ee
Илья Евсеев <Ilya.Evseev_ext@lemanapro.ru>
2025-02-14 12:56:09 +0400

    14

---
+++ lua/plugins/utils/indent.lua
@@ -0,0 +1,18 @@
+-- return {
+--   "lukas-reineke/indent-blankline.nvim",
+--   enabled= require('nixCatsUtils').lazyAdd(true,true),
+--   event = {"BufReadPost", "BufNewFile"},
+--   main = "ibl",
+--   opts = {
+--   indent = { highlight = highlight, char = "▏" }, }
+-- }
+return {
+  "folke/snacks.nvim",
+enabled= require('nixCatsUtils').lazyAdd(true,true),
+  event = {"BufReadPost", "BufNewFile"},
+  opts = {
+    indent={
+      char = "▏"
+    }
+  },
+}
