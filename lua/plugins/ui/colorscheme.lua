a44d48e45f2b4bcaa11ba555450703d9c544686c
Илья Евсеев <Ilya.Evseev_ext@lemanapro.ru>
2025-02-14 10:14:08 +0400

    10

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin-nvim',
-  dev = require('nixCatsUtils').lazyAdd(false, true),
+  dev = require('nixCatsUtils').lazyAdd(true, true),
   config = function()
     vim.cmd.colorscheme 'catppuccin-mocha'
   end,
