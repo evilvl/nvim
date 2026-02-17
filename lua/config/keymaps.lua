6243b7fa1ee19c86d2f21039d648a9baf043ff9b
Илья Евсеев <Ilya.Evseev_ext@lemanapro.ru>
2026-02-17 22:14:35 +0400

    30

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- clipboard
-vim.opt.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
 
 -- Clear search when pressing <Esc>
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
