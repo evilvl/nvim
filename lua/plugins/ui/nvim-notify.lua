194cb7d5b5bad8d0ea979805669ef8b361053da5
Илья Евсеев <Ilya.Evseev_ext@lemanapro.ru>
2026-02-18 14:27:34 +0400

    32 little fix

--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -2,22 +2,22 @@ return {
 	"rcarriga/nvim-notify",
 	event = "VeryLazy",
 	opts = {
-		fps = 60,
+		fps = 144,
 		render = "wrapped-compact",
 		stages = "fade_in_slide_out",
 
-		timeout = 3000,
+		timeout = 1000,
 		top_down = false,
 		minimum_width = 40,
 
-		background_colour = "#000000",
+		-- background_colour = "#000000",
 
 		icons = {
-			DEBUG = "",
-			ERROR = "",
-			INFO = "",
-			TRACE = "✎",
-			WARN = "",
+			DEBUG = "",
+			ERROR = "",
+			INFO = "",
+			TRACE = "",
+			WARN = "",
 		},
 	},
 }
