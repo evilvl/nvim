9bf25a8943b8435edfa1b40abedf00f5b234793f
evilvl <ev5eevya@ya.ru>
2026-08-06 19:56:50 +0400

    fix <>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,7 @@ vim.o.clipboard = "unnamedplus"
 
 -- Clear search when pressing <Esc>
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Not exit visual mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
