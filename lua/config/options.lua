457514098cdd31552206947d8a23c1e7d737e560
evilvl <ev5eevya@ya.ru>
2026-08-06 19:09:06 +0400

    fix tab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Tab Settings
-vim.o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.o.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
 vim.o.expandtab = true -- Convert tabs to spaces
-vim.o.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
-vim.o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.o.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.o.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
 
 -- Line Wrapping
 vim.opt.wrap = true -- Enable line wrapping
