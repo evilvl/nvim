5f239a28b1c44859b215f313f2daf988c4544113
evilvl <ev5eevya@ya.ru>
2026-08-06 17:48:46 +0400

    upd

--- lua/plugins/formatting/conform.lua
+++ lua/plugins/formatting/conform.lua
@@ -26,6 +26,7 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			nix = { "nixfmt" },
+      kdl = { "kdlfmt" },
 
 			c = { "clang-format" },
 			cpp = { "clang-format" },
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -23,6 +23,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
       ensure_installed = {
+        "kdlfmt",
         "stylua",
         "shellcheck",
         "shfmt",
